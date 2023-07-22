8a7cbe93f70ae33d48686289b5a89a561397da99
DrowskoytayhulGuider <77562801+DrowskoytayhulGuider@users.noreply.github.com>
2023-07-22 10:34:29 +0800

    Update DingZhen_cmd.py

--- DingZhen_cmd.py
+++ DingZhen_cmd.py
@@ -1,10 +1,5 @@
 #coding: gbk
 from main.bingAI import BingDingZhen
-from main.gui import GUI
-import wx
 
 if __name__ == '__main__':
-    #BingDingZhen()
-    app = wx.App()
-    GUI(parent=None, id=-1).Show()
-    app.MainLoop()
+    BingDingZhen()
